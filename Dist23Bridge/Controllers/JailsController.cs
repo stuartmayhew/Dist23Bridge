346740a84c6c6eef6016087d1414fde66dfeecab
stuartmayhew <stumay111@gmail.com>
2016-03-24 11:21:58 -0500

    Add pages etc

---
+++ Dist23Bridge/Controllers/JailsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Dist23Bridge;
+
+namespace Dist23Bridge.Controllers
+{
+    public class JailsController : Controller
+    {
+        private Dist23BridgeEntities db = new Dist23BridgeEntities();
+
+        // GET: Jails
+        public ActionResult Index()
+        {
+            return View(db.Jails.ToList());
+        }
+
+        // GET: Jails/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jail jail = db.Jails.Find(id);
+            if (jail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jail);
+        }
+
+        // GET: Jails/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Jails/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "jail_id,JailName,JailAddress,JailContact,JailContPhone,JailContEmail")] Jail jail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Jails.Add(jail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(jail);
+        }
+
+        // GET: Jails/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jail jail = db.Jails.Find(id);
+            if (jail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jail);
+        }
+
+        // POST: Jails/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "jail_id,JailName,JailAddress,JailContact,JailContPhone,JailContEmail")] Jail jail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(jail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(jail);
+        }
+
+        // GET: Jails/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jail jail = db.Jails.Find(id);
+            if (jail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jail);
+        }
+
+        // POST: Jails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Jail jail = db.Jails.Find(id);
+            db.Jails.Remove(jail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
